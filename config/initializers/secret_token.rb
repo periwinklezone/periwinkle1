146e99b16808caac170abb27acbe7728c32bc53a
Periwinkle <periwinklezone@gmail.com>
2010-11-12 12:46:29 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Periwinkle1::Application.config.secret_token = '6530eaf840d845d056c4304aaa57ab2a33b3cc4c9c3f628a7fb89884afcea89785f3e4ebfa484d41979dd9716b19a05fffa9ef7ff1be526697c720e9cbec42c2'
